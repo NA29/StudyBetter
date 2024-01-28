9b8a828d66188bd1e10bc32fbdbc867615c9da42
Eddie Chen <147354568+eddiechn@users.noreply.github.com>
2024-01-27 21:11:27 -0500

    output in .txt file

--- Model3/scripts/demo.py
+++ Model3/scripts/demo.py
@@ -1,35 +1,35 @@
 import json
-
 import cv2
-import matplotlib.pyplot as plt
 from path import Path
-
 from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig, ReaderConfig, PrefixTree
+import matplotlib.pyplot as plt
 
-with open('Model3\data\config.json') as f:
-    sample_config = json.load(f)
+# Open a text file for writing
+with open('output.txt', 'w') as file:
+    with open('Model3\data\config.json') as f:
+        sample_config = json.load(f)
 
-with open('Model3\data\words_alpha.txt') as f:
-    word_list = [w.strip().upper() for w in f.readlines()]
-prefix_tree = PrefixTree(word_list)
+    with open('Model3\data\words_alpha.txt') as f:
+        word_list = [w.strip().upper() for w in f.readlines()]
+    prefix_tree = PrefixTree(word_list)
 
-for decoder in ['best_path', 'word_beam_search']:
-    for img_filename in Path('Model3\data').files('*.png'):
-        print(f'Reading file {img_filename} with decoder {decoder}')
+    for decoder in ['best_path', 'word_beam_search']:
+        for img_filename in Path('Model3\data').files('*.png'):
+            file.write(f'Reading file {img_filename} with decoder {decoder}\n')
 
-        # read text
-        img = cv2.imread(img_filename, cv2.IMREAD_GRAYSCALE)
-        scale = sample_config[img_filename.basename()]['scale'] if img_filename.basename() in sample_config else 1
-        margin = sample_config[img_filename.basename()]['margin'] if img_filename.basename() in sample_config else 0
-        read_lines = read_page(img,
-                               detector_config=DetectorConfig(scale=scale, margin=margin),
-                               line_clustering_config=LineClusteringConfig(min_words_per_line=2),
-                               reader_config=ReaderConfig(decoder=decoder, prefix_tree=prefix_tree))
+            # read text
+            img = cv2.imread(img_filename, cv2.IMREAD_GRAYSCALE)
+            scale = sample_config[img_filename.basename()]['scale'] if img_filename.basename() in sample_config else 1
+            margin = sample_config[img_filename.basename()]['margin'] if img_filename.basename() in sample_config else 0
+            read_lines = read_page(img,
+                                   detector_config=DetectorConfig(scale=scale, margin=margin),
+                                   line_clustering_config=LineClusteringConfig(min_words_per_line=2),
+                                   reader_config=ReaderConfig(decoder=decoder, prefix_tree=prefix_tree))
 
-        # output text
-        for read_line in read_lines:
-            print(' '.join(read_word.text for read_word in read_line))
-        print()
+            # output text to file
+            for read_line in read_lines:
+                file.write(' '.join(read_word.text for read_word in read_line) + '\n')
+            file.write('\n')
 
         # plot image with detections and texts as overlay
         plt.figure(f'Image: {img_filename} Decoder: {decoder}')
